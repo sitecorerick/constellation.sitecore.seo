140fb4a287dc025e92da2458fcbe5a477788e466
Richard Cabral <richard@viridianspark.com>
2013-08-29 13:01:59 -0400

    Migrated Sitemaps namespace to this project.

---
+++ HttpHandlers/RobotsTxtHandler.cs
@@ -0,0 +1,41 @@
+﻿namespace Spark.Sitecore.Seo.Sitemaps.HttpHandlers
+{
+	using System.Text;
+	using System.Web;
+
+	/// <summary>
+	/// Handles requests for robots.txt and response with a reference to the hostname-specific
+	/// sitemap.xml file.
+	/// </summary>
+	public class RobotsTxtHandler : IHttpHandler
+	{
+		#region IHttpHandler Members
+		/// <summary>
+		/// Gets a value indicating whether this instance of the handler is reusable.
+		/// Required by IHttpHandler, not used by developers in this case.
+		/// </summary>
+		public bool IsReusable
+		{
+			get { return false; }
+		}
+
+		/// <summary>
+		/// Required by IHttpHandler, this is called by ASP.NET when an appropriate URL match is found.
+		/// </summary>
+		/// <param name="context">The current request context.</param>
+		public void ProcessRequest(HttpContext context)
+		{
+			var builder = new StringBuilder();
+
+			builder.AppendLine("User-agent: *");
+			builder.AppendLine();
+			builder.AppendLine("Sitemap: " + context.Request.Url.GetLeftPart(System.UriPartial.Authority) + "/sitemap.xml");
+
+			context.Response.Clear();
+			context.Response.ContentType = "text";
+			context.Response.Write(builder.ToString());
+			context.Response.End();
+		}
+		#endregion
+	}
+}
---
+++ Sitemaps/ICrawler.cs
@@ -0,0 +1,24 @@
+﻿namespace Spark.Sitecore.Seo.Sitemaps
+{
+	using System.Xml;
+
+	using global::Sitecore.Sites;
+
+	/// <summary>
+	/// The base contract for an object that represents a crawler in a sitemap.xml document.
+	/// The crawler object is used to build the sitemap
+	/// </summary>
+	public interface ICrawler
+	{
+		/// <summary>
+		/// The crawl.
+		/// </summary>
+		/// <param name="site">
+		/// The site.
+		/// </param>
+		/// <param name="doc">
+		/// The doc.
+		/// </param>
+		void Crawl(SiteContext site, XmlDocument doc);
+	}
+}
